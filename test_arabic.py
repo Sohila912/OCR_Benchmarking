f19513384a066c65fa5db7de7563418256506142
Sohila912 <sohilahosam201@gmail.com>
2026-07-13 13:39:53 +0300

    Reformatting outputs

--- test_arabic.py
+++ test_arabic.py
@@ -8,7 +8,7 @@ import re
 # ======================================================
 
 GROUND_TRUTH = Path("Datasets/Arabic/Markdown_Reference")
-OCR_OUTPUT = Path("Outputs/Arabic/paddleocr")
+OCR_OUTPUT = Path("Outputs/Arabic/markerocr")
 
 # ======================================================
 
@@ -177,7 +177,7 @@ for ref_file in reference_files:
 
 df = pd.DataFrame(results)
 
-df.to_excel("Evaluation Results/Arabic/Paddle_OCR_Arabic_Evaluation.xlsx", index=False)
+df.to_excel("Evaluation Results/Arabic/Marker_OCR_Arabic_Evaluation.xlsx", index=False)
 
 print(df)
 
